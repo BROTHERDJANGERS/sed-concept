780438f97e10e4290dd571dc009fc3cec126a69f
Alex Sachkov <sachkov3@gmail.com>
2021-05-05 22:48:31 +0300

    add count number of docs

--- SED/home/views.py
+++ SED/home/views.py
@@ -1,9 +1,16 @@
+import os
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
 def home(request):
+    if request.method == 'GET':
+        files = os.listdir(path="./media")
+        count = len(files)
+        return render(request, 'home/home.html', {
+            'count': count
+           })   
     return render(request,'home/home.html')
 
 
@@ -16,7 +23,7 @@ def upload(request):
         filename = fs.save(file.name, file)
         # получение адреса по которому лежит файл
         file_url = fs.url(filename)
-        return render(request, 'home/home.html', {
+        return render(request, 'home/upload.html', {
             'file_url': file_url
         })
     return render(request,'home/upload.html')
