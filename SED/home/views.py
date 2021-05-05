8f1da24b18bc21b065d2fbfd840325f927e4e2c6
Alex Sachkov <sachkov3@gmail.com>
2021-05-05 22:20:34 +0300

    make greate again upload

--- SED/home/views.py
+++ SED/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
@@ -7,4 +8,15 @@ def home(request):
 
 
 def upload(request):
+    if request.method == 'POST' and request.FILES:
+        # получаем загруженный файл
+        file = request.FILES['myfile']
+        fs = FileSystemStorage()
+        # сохраняем на файловой системе
+        filename = fs.save(file.name, file)
+        # получение адреса по которому лежит файл
+        file_url = fs.url(filename)
+        return render(request, 'home/home.html', {
+            'file_url': file_url
+        })
     return render(request,'home/upload.html')
