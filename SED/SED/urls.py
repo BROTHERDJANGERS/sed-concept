263c94c114a83b3eb343498e00c73518ad63378a
Alex Sachkov <sachkov3@gmail.com>
2021-05-05 13:02:34 +0300

    begin auth

--- SED/SED/urls.py
+++ SED/SED/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from SEDmenu import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('accounts/', include('django.contrib.auth.urls')), # Добавили новый маршрут
     path('upload/', views.upload),
     path('admin/', admin.site.urls),
 
