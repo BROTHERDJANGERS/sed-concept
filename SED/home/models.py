173ad585970de0a418f0aaba29c96cc049c06c80
Alex Sachkov <sachkov3@gmail.com>
2021-05-08 17:31:52 +0300

    визуал и аплоад

--- SED/home/models.py
+++ SED/home/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+#def user_directory_path(instance, filename):    
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+#    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 class Doc(models.Model):
+    #user = models.ForeignKey(to=User, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=150)
-    doc = models.FileField(upload_to='docs/')
+    user_upload = models.CharField(max_length=150,null=True)
+    #doc = models.FileField(upload_to="docs/")
 
     def __str__(self):
         return self.title
